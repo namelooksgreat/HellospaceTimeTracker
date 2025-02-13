e71d18db3b548615548abdc08988ac231c6a5757
namelooksgreat <samet@hellospace.world>
2025-02-13 18:33:35 +0000

    Build Ready Version 1.6

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL ||
+  "https://tykqnnygrqovcbqbkszp.supabase.co";
+const supabaseAnonKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5a3FubnlncnFvdmNicWJrc3pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1MDMwNzMsImV4cCI6MjA1NDA3OTA3M30.a09sUrHIFTVUM0PeL5apxByWEF1K_-qDDLY-WMX3bvw";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error("Missing Supabase environment variables:", {
