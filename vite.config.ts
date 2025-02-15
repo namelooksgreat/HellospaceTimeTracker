9bd8cff1571ac8e6d42651bc40370a5af7ed6d89
namelooksgreat <samet@hellospace.world>
2025-02-15 21:45:40 +0000

    v5.4.46

--- vite.config.ts
+++ vite.config.ts
@@ -3,66 +3,72 @@ import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
 import path from "path";
 
-export default defineConfig({
-  plugins: [
-    react({
-      plugins: process.env.TEMPO === "true" ? [["tempo-devtools/swc", {}]] : [],
-      jsxImportSource: "react",
-      fastRefresh: true,
-    }),
-    tempo(),
-  ],
-  optimizeDeps: {
-    exclude: ["tempo-routes"],
-    include: ["react", "react-dom", "react-router-dom", "framer-motion"],
-  },
-  base: "/",
-  server: {
-    // @ts-ignore
-    allowedHosts: process.env.TEMPO === "true" ? true : undefined,
-    host: true,
-    port: 3000,
-  },
-  build: {
-    sourcemap: false,
-    minify: "terser",
-    chunkSizeWarningLimit: 2000,
-    target: "esnext",
-    terserOptions: {
-      compress: {
-        drop_console: false,
-        drop_debugger: false,
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      react({
+        plugins:
+          process.env.TEMPO === "true" ? [["tempo-devtools/swc", {}]] : [],
+        jsxImportSource: "react",
+        fastRefresh: true,
+      }),
+      tempo(),
+    ],
+    optimizeDeps: {
+      exclude: ["tempo-routes"],
+      include: ["react", "react-dom", "react-router-dom", "framer-motion"],
+    },
+    base: "/",
+    server: {
+      // @ts-ignore
+      allowedHosts: process.env.TEMPO === "true" ? true : undefined,
+      host: true,
+      port: 3000,
+    },
+    build: {
+      sourcemap: false,
+      minify: "terser",
+      chunkSizeWarningLimit: 2000,
+      target: "esnext",
+      terserOptions: {
+        compress: {
+          drop_console: false,
+          drop_debugger: false,
+        },
+      },
+      modulePreload: {
+        polyfill: true,
+      },
+      rollupOptions: {
+        external: ["tempo-routes"],
+        output: {
+          paths: {
+            "tempo-routes": "/tempo-routes",
+          },
+          manualChunks: {
+            vendor: ["react", "react-dom", "react-router-dom", "framer-motion"],
+            ui: [
+              "@radix-ui/react-portal",
+              "@radix-ui/react-dismissable-layer",
+              "@radix-ui/react-dialog",
+              "@radix-ui/react-select",
+            ],
+          },
+          format: "es",
+          entryFileNames: "[name].[hash].mjs",
+          chunkFileNames: "[name].[hash].mjs",
+          assetFileNames: "assets/[name].[hash][extname]",
+        },
       },
     },
-    modulePreload: {
-      polyfill: true,
-    },
-    rollupOptions: {
-      external: ["tempo-routes"],
-      output: {
-        paths: {
-          "tempo-routes": "/tempo-routes",
-        },
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom", "framer-motion"],
-          ui: [
-            "@radix-ui/react-portal",
-            "@radix-ui/react-dismissable-layer",
-            "@radix-ui/react-dialog",
-            "@radix-ui/react-select",
-          ],
-        },
-        format: "es",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      global: "globalThis",
     },
-  },
-  define: {
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    global: "globalThis",
-  },
+  };
 });
